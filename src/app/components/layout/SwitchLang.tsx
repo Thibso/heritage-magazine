8e5713827f09282149fe87215ab7ca9ce166e5ec
Thibso <thibautsoulet33@gmail.com>
2025-05-23 17:09:01 +0200

    url correction lang switch

--- src/app/components/layout/SwitchLang.tsx
+++ src/app/components/layout/SwitchLang.tsx
@@ -4,7 +4,8 @@ import en from "@/app/img/en.png";
 import fr from "@/app/img/fr.png";
 import Image from "next/image";
 
-import { usePathname } from "@/i18n/routing";
+import { getPathname, usePathname } from "@/i18n/routing";
+
 import Link from "next/link";
 
 type Props = {
@@ -14,10 +15,20 @@ type Props = {
 export default function SwitchLang(props: Props) {
   const pathname = usePathname();
 
+  const translatePathname_FR = getPathname({
+    locale: "fr",
+    href: pathname,
+  });
+
+  const translatePathname_EN = getPathname({
+    locale: "en",
+    href: pathname,
+  });
+
   return (
     <div>
       {props.locale === "en" ? (
-        <Link href={"/fr" + pathname} title="French version">
+        <Link href={translatePathname_FR} hrefLang="fr" title="French version">
           <Image
             className="max-lg:max-h-[32px] max-lg:w-fit lg:h-[24px] object-contain"
             src={fr}
@@ -25,7 +36,11 @@ export default function SwitchLang(props: Props) {
           />
         </Link>
       ) : (
-        <Link href={"/en" + pathname} title="Version Anglaise">
+        <Link
+          href={translatePathname_EN}
+          hrefLang="en"
+          title="Version Anglaise"
+        >
           <Image
             className="max-lg:max-h-[32px] max-lg:w-fit lg:h-[24px] object-contain"
             src={en}
--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,4 +1,4 @@
-import { createSharedPathnamesNavigation } from "next-intl/navigation";
+import { createNavigation } from "next-intl/navigation";
 import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
@@ -135,5 +135,5 @@ export const routing = defineRouting({
 
 // Lightweight wrappers around Next.js' navigation APIs
 // that will consider the routing configuration
-export const { Link, redirect, usePathname, useRouter } =
-  createSharedPathnamesNavigation(routing);
+export const { Link, redirect, usePathname, useRouter, getPathname } =
+  createNavigation(routing);
