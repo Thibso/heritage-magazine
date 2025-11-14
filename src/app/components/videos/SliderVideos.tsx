1e235f915e1d783a34520a97f2bff12d3193cefe
Thibso <thibautsoulet33@gmail.com>
2025-11-14 17:54:39 +0100

    interview home

--- src/app/components/videos/SliderVideos.tsx
+++ src/app/components/videos/SliderVideos.tsx
@@ -4,10 +4,13 @@ import { motion } from "motion/react";
 
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
+import { useMediaQuery } from "react-responsive";
 
 export default function SliderVideos() {
   const [active, setActive] = useState(0);
   const videoRefs = useRef<(HTMLVideoElement | null)[]>([]);
+
+  const isMobile = useMediaQuery({ query: "(max-width: 1024px)" });
 
   // VIDEOS URLS ARRAY
   const videos = [
@@ -40,7 +43,7 @@ export default function SliderVideos() {
 
   return (
     // BIG CONTAINER
-    <div className="relative w-full z-[1] py-8 lg:py-24">
+    <div className="relative w-full z-[1] pt-8 pb-16 lg:py-24">
       {/* DEBUG */}
       {/* <div className="absolute top-0 left-1/2 -translate-x-1/2 h-full w-2 bg-[rgb(220,38,38)] z-[99]"></div> */}
       {/*  */}
@@ -48,19 +51,21 @@ export default function SliderVideos() {
       {/* SLIDER */}
       <motion.div
         animate={{
-          x: -15.5 - active * 34.5 + "%",
+          x: isMobile ? -25 - active * 34.5 + "%" : -15.5 - active * 34.5 + "%",
         }}
         transition={{
           duration: 0.4,
           ease: "easeInOut",
         }}
-        className={"flex flex-nowrap gap-8 lg:gap-20 w-[145%] min-w-[145%]"}
+        className={
+          "flex flex-nowrap gap-8 lg:gap-20 w-[200%] min-w-[200%] lg:w-[145%] lg:min-w-[145%]"
+        }
       >
         {videos.map((video, index) => {
           return (
             <article
               key={"video-" + index}
-              className={`${index - 1 === active ? "scale-[1.2]" : "scale-[1]"} transition-all relative z-[1]`}
+              className={`${index - 1 === active ? "scale-[1.15]" : "scale-[0.95]"} transition-all relative z-[1]`}
               onClick={() => {
                 setActive(index - 1);
                 handleVideoClick(index, false);
@@ -85,7 +90,7 @@ export default function SliderVideos() {
 
       {/* CONTROLS */}
       <button
-        className="absolute left-1/2 -translate-x-[30px] lg:-translate-x-[150px] bottom-0 bg-mainBlue disabled:bg-mainBlue/70 desabled:cursor-not-allowed rounded-full p-2 z-[99]"
+        className="absolute left-[20%] lg:left-1/2 lg:-translate-x-[150px] bottom-0 bg-mainBlue disabled:bg-mainBlue/70 desabled:cursor-not-allowed rounded-full p-2 z-[99]"
         disabled={active === -1}
         onClick={() => {
           setActive((current) => {
@@ -98,7 +103,7 @@ export default function SliderVideos() {
         <ChevronLeft className="size-6 text-orange" />
       </button>
       <button
-        className="absolute left-1/2 translate-x-[30px] lg:translate-x-[150px] bottom-0 bg-mainBlue disabled:bg-mainBlue/70 desabled:cursor-not-allowed rounded-full p-2 z-[99]"
+        className="absolute max-lg:right-[20%] lg:left-1/2 lg:translate-x-[150px] bottom-0 bg-mainBlue disabled:bg-mainBlue/70 desabled:cursor-not-allowed rounded-full p-2 z-[99]"
         disabled={active === 1}
         onClick={() => {
           setActive((current) => {
