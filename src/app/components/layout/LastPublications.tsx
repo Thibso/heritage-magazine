4695ddc59fa077b3c323f5b051f65c621687b4d5
Thibso <thibautsoulet33@gmail.com>
2026-02-05 14:25:37 +0100

    test json

--- src/app/components/layout/LastPublications.tsx
+++ src/app/components/layout/LastPublications.tsx
@@ -1,26 +1,74 @@
-import fs from "fs";
 import { useTranslations } from "next-intl";
-import path from "path";
 import Button from "../buttons/Button";
 import LastPublicationsSlider from "./LastPublicationsSlider";
 
-type Item = {
-  image: string;
-  head: string;
-  alt: string;
-  h2: string;
-  h3: string;
-  p: string;
-  linkTo: string;
-  linkText: string;
-  tags: string[];
-};
-
 export default function LastPublications() {
   const t = useTranslations("HomePage");
-  const filePath = path.join(process.cwd(), "public/lastpublications.json");
-  const jsonData = fs.readFileSync(filePath, "utf-8");
-  const data: Item[] = JSON.parse(jsonData);
+
+  const data = [
+    {
+      image: "",
+      head: "/publications/skinceuticals-aesthetic-media-heritage-magazine.jpg",
+      alt: "Skinceuticals HERITAGE Magazine",
+      h2: "Prague_Skinceuticals.h2",
+      h3: "Prague_Skinceuticals.h3",
+      p: "Prague_Skinceuticals.content",
+      linkTo: "Prague_Skinceuticals.cta_link",
+      linkText: "Prague_Skinceuticals.cta",
+      tags: ["Prague_Skinceuticals.tags.t1.title"],
+    },
+    {
+      image: "",
+      head: "/publications/biosolution-co-exosomes-specialiste-aesthetic-magazine.jpg",
+      alt: "Biosolution.co HERITAGE Magazine",
+      h2: "Prague_Exosomes.h2",
+      h3: "Prague_Exosomes.h3",
+      p: "Prague_Exosomes.content",
+      linkTo: "Prague_Exosomes.cta_link",
+      linkText: "Prague_Exosomes.cta",
+      tags: ["Prague_Exosomes.tags.t1.title"],
+    },
+    {
+      image: "/contributors/Osvaldova-eva-heritage-magazine.png",
+      head: "/publications/eva-osvaldova-julien-vervel-specialists-aesthetic-magazine.jpg",
+      alt: "Eva Osvaldova & Julien Vervel HERITAGE Magazine",
+      h2: "Prague_Eva_Osvaldova_Healthcie.h2",
+      h3: "Prague_Eva_Osvaldova_Healthcie.h3",
+      p: "Prague_Eva_Osvaldova_Healthcie.content",
+      linkTo: "Prague_Eva_Osvaldova_Healthcie.cta_link",
+      linkText: "Prague_Eva_Osvaldova_Healthcie.cta",
+      tags: [
+        "Prague_Eva_Osvaldova_Healthcie.tags.t1.title",
+        "Prague_Eva_Osvaldova_Healthcie.tags.t2.title",
+        "Prague_Eva_Osvaldova_Healthcie.tags.t3.title",
+      ],
+    },
+    {
+      image: "/contributors/nivault-hugo-healthcie-heritage-magazine.png",
+      head: "/publications/hugo-nivault-medical-website-specialiste-aesthetic-media.jpg",
+      alt: "HEALTHCIE HERITAGE Magazine",
+      h2: "Prague_Healthcie.h2",
+      h3: "Prague_Healthcie.h3",
+      p: "Prague_Healthcie.content",
+      linkTo: "Prague_Healthcie.cta_link",
+      linkText: "Prague_Healthcie.cta",
+      tags: [
+        "Prague_Healthcie.tags.t1.title",
+        "Prague_Healthcie.tags.t2.title",
+      ],
+    },
+    {
+      image: "/contributors/steven-harris-heritage-magazine.png",
+      head: "/publications/instagram-vs-reality-aesthetic-specialist-heritage-magazine.jpg",
+      alt: "Steven Harris HERITAGE Magazine",
+      h2: "Prague_Steven_Harris.h2",
+      h3: "Prague_Steven_Harris.h3",
+      p: "Prague_Steven_Harris.content",
+      linkTo: "Prague_Steven_Harris.cta_link",
+      linkText: "Prague_Steven_Harris.cta",
+      tags: ["Prague_Steven_Harris.tags.t1.title"],
+    },
+  ];
 
   return (
     <section className="overflow-hidden  m-auto bg-bgLight">
